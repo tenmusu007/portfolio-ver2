5606561959fa5599a330e728c24de42645aaf0ca
Atsuya Tanaka <tenmusu007@gmail.com>
2022-08-16 14:05:58 -0700

    add index.css

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import { LinkProvider } from "./useContext/linkContext"
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LinkProvider>
-      <App />
-    </LinkProvider>
+    <App />
   </React.StrictMode>
 );
 
