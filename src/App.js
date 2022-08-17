143be7cff766004ae76fdf560670c8ce08e5e4c3
Atsuya Tanaka <tenmusu007@gmail.com>
2022-08-17 03:26:21 -0700

    delete cstyled compornent

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Home from './components/Home';
 import DetailWork from './components/DetailWork';
 import { LinkProvider } from "./useContext/linkContext"
 import data from './workData/data';
-import { Header } from './components/Header';
-import { Intro } from './components/Intro';
 
 
 function App() {
@@ -14,8 +12,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/header" element={<Header />} />
-          <Route path="/intro" element={<Intro />} />
           <Route path={'/:id'} element={<DetailWork data={data} />} />
         </Routes>
       </BrowserRouter>
