c216d3f46cc456f84e96e080155c30ec658c2693
Atsuya Tanaka <tenmusu007@gmail.com>
2022-08-16 20:40:12 -0700

    add styled components directly

--- src/components/AboutIntro.js
+++ src/components/AboutIntro.js
@@ -1,8 +1,33 @@
 import React, { useState } from "react";
 import { Grid, Button } from "@mui/material";
-import { StyledAboutintro } from "../styles/Aboutintro.styled";
+// import { StyledAboutintro } from "../styles/Aboutintro.styled";
+import styled from "styled-components";
 import { motion } from "framer-motion";
 import img from "../img/myself.jpeg";
+const StyledAboutintro = styled.div`
+	@media only screen and (max-width: 900px) {
+		.mobileGrid {
+			display: flex;
+			flex-direction: column-reverse;
+		}
+	}
+	.aboutIntroContainer {
+		text-align: center;
+	}
+	.extraTxt {
+		margin-top: 5px;
+		padding: 30px;
+		display: block;
+		line-height: 2;
+		text-align: left;
+		position: relative;
+		width: 100%;
+	}
+	.moreRead {
+		margin-top: 20px;
+		color: white;
+	}
+`;
 
 export const AboutIntro = () => {
 	const [modal, setModal] = useState(true);
